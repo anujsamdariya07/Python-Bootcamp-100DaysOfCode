e077150c282857850f0d32d595f222849bf1bd7c
anujsamdariya07 <anujsamdariya07@gmail.com>
2024-08-27 20:05:30 +0530

    Day-1 Complete✅

---
+++ Day-1/Project.py
@@ -0,0 +1,8 @@
+# Enter your name
+name = input('What is your name?\n')
+
+# Enter the name of your pet
+pet = input('What is the name of your pet?\n')
+
+# Combine your name and pet to make the name of the band
+print(f'Your band name could be {name} {pet}!')
--- Day-1/task.py
+++ Day-1/task.py
@@ -1,2 +1,17 @@
 # Write your code down below
-print("Hello World!")
+# print('Hello World!')
+
+# print('Hello World!\n')
+
+# String concatenation
+# print('Hello' + ' ' + 'World' + '!')
+
+# input() function
+# print(f'Hello👋 {input("What is your name?")}!')
+
+# Variable
+name = input("What is your name?")
+print(f'Hello👋, {name}!')
+
+length = len(name)
+print(f'The length of your name is {length}!')
